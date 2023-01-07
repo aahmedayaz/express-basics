6bb18f2412ae70c5026ece08da0ffc4d4239eaca
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2023-01-07 19:34:00 +0500

    added all crud

--- server.js
+++ server.js
@@ -1,93 +1,142 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
 dotenv.config();
 
 const app = express();
-
 app.use(express.json({ extended: false }));
-
 const PORT = process.env.PORT || 5000;
 
-const db = [
-  {
-    name: "Ahmed Ayaz",
-    age: 20,
-  },
-  {
-    name: "Ahmed Faraz",
-    age: 20,
-  },
-  {
-    name: "Rashid",
-    age: 20,
-  },
+let users_database = [
+  { id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed", name: "Ahmed Ayaz", age: 20 },
+  { id: "2b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed", name: "Ahmed Faraz", age: 20 },
+  { id: "3b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed", name: "Rashid", age: 40 },
 ];
 
 // @route   GET /
 // @desc    Get greetings message
 // @access  Public
 app.get("/", (req, res) => {
-  res.send(`Welcome to backend app developed by ${process.env.NAME}`);
+  return res.send(`Welcome to backend app developed by ${process.env.NAME}`);
 });
 
 // ================================================================================
 // User CRUD
 // ================================================================================
 
-// @route   GET /api
+// @route   GET /users
 // @desc    Get all Users
 // @access  Public
-app.get("/api", (req, res) => {
+app.get("/users", (req, res) => {
   try {
-    res.status(200).send(db);
+    return res.status(200).send(users_database);
   } catch (error) {
-    res.status(500).send("Server Error");
+    return res.status(500).send("Server Error");
   }
 });
 
-// @route   POST /api
-// @desc    Add a User
+// @route   GET /users/:id
+// @desc    Get a user
 // @access  Public
-app.post("/api", (req, res) => {
+app.get("/users/:id", (req, res) => {
   try {
-    let { name, age } = req.body;
-    console.log(req.body);
-    // const name = req.body.name;
-    // const age = req.body.age;
-    name = name?.trim();
+    const id = req.params.id;
+    const user = users_database.filter((user) => user.id === id)[0];
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(500).send("Server Error");
+  }
+});
 
+// @route   POST /users
+// @desc    Add all Users
+// @access  Public
+app.post("/users", (req, res) => {
+  try {
+    // 1. Get user from body
+    let { name, age } = req.body;
+
+    // 2. Validate data
+    name = name?.trim();
     if (!name || name == "") {
       return res.status(400).send("Bad Request: Name is missing.");
     }
     if (!age) {
       return res.status(400).send("Bad Request: Age is missing.");
     }
-    db.push({ name, age });
-    res.status(200).send(db);
+
+    // 3. Add user inside database
+    const id = uuidv4();
+    users_database.push({ id, name, age });
+
+    // 4. send response
+    return res.status(200).send(users_database);
   } catch (error) {
-    res.status(500).send("Server Error");
+    return res.status(500).send("Server Error");
   }
 });
 
-// @route   GET /api
-// @desc    Get all Users
+// @route   PUT /users/:id
+// @desc    Update User
 // @access  Public
-app.get("/api", (req, res) => {
+app.put("/users/:id", (req, res) => {
   try {
-    res.status(200).send(db);
+    // 1. Get user from body
+    let { name, age } = req.body;
+
+    // 2. Validate data
+    name = name?.trim();
+    if (!name || name == "") {
+      return res.status(400).send("Bad Request: Name is missing.");
+    }
+    if (!age) {
+      return res.status(400).send("Bad Request: Age is missing.");
+    }
+
+    // 3. Update user
+    const id = req.params.id;
+    users_database = users_database.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+          age,
+        };
+      } else {
+        return user;
+      }
+    });
+
+    // 4. send response
+    return res.status(200).send(users_database);
   } catch (error) {
-    res.status(500).send("Server Error");
+    return res.status(500).send("Server Error");
   }
 });
 
-// @route   GET /api
-// @desc    Get all Users
+// @route   DELETE /users/:id
+// @desc    Delete User
 // @access  Public
-app.get("/api", (req, res) => {
+app.delete("/users/:id", (req, res) => {
   try {
-    res.status(200).send(db);
+    const id = req.params.id;
+    let found = false;
+
+    users_database.forEach((user) => {
+      if (user.id === id) {
+        found = true;
+      }
+    });
+
+    if (!found) {
+      return res.status(400).send("Bad Request: User not found.");
+    }
+
+    users_database = users_database.filter((user) => user.id !== id);
+
+    return res.status(200).send("User has been deleted.");
   } catch (error) {
-    res.status(500).send("Server Error");
+    return res.status(500).send("Server Error");
   }
 });
 
